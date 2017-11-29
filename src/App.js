6abf22f7ec789eb60b4c87ce8e6bf7522c933616
Michael <Michael.Antczak@gmail.com>
2017-11-29 16:02:12 +0000

    Connect to firebase
    	- yarn firebase
    	- fetch the cakes from firebase
    	- populate the state with cakes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,57 @@ import './App.css';
 import firebase from './config/firebase.js';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cakes: [], 
+    }
+  }
+
+  componentDidMount() {
+    
+        // Get a reference to the database service
+        const database = firebase.database();
+    
+        /****************************
+          fetch cakes from Firebase
+        *****************************/
+        const cakes = database.ref('cakes');
+    
+        // this sets an event on change in Firebase, so 
+        // will be updated automagically when there is 
+        // an update in Firebase
+        cakes.on('value', (snapshot) => {
+    
+          // convert Firebase object into an Array and add to state
+          const allCakes = snapshot.val();
+          console.log(allCakes);
+          let newState = [];
+    
+          for(let cake in allCakes) {
+            newState.push({
+              name: allCakes[cake].name,
+              imageUrl: allCakes[cake].imageUrl,
+              comment: allCakes[cake].comment,
+              yumFactor: allCakes[cake].yumFactor,
+            });
+            console.log(`
+              ${allCakes[cake].name}
+              ${allCakes[cake].imageUrl}
+              ${allCakes[cake].comment}
+              ${allCakes[cake].yumFactor}
+            `)
+          }
+     
+          this.setState({
+            cakes: newState
+          });
+        });
+    
+
+      }  // end of componentDidMount
+
   render() {
     return (
       <div className="App">
