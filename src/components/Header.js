0a4829ccf83a01d4fdcbcc53ec7e9de5e51983b1
Michael <Michael.Antczak@gmail.com>
2017-11-29 16:22:26 +0000

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="headerContainer">
+            <h1>
+                <span className="bold">CAKE</span>
+                <span className="thin">LOVE</span>
+            </h1>
+        </div>
+    )
+}
+
+export default Header; 
